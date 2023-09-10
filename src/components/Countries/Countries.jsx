10a40e4915d3a8d4f4a86196a0b225c1205c1be0
unknown <ahnaf.ahmed.42173@gmail.com>
2023-09-10 22:45:37 +0600

    added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -12,14 +12,35 @@ const Countries = () => {
         .then(data => setCountries(data))
     })
 
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountry = country =>{
+        console.log('added');
+        console.log(country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+
+    }
+
 
     return (
+
         <div >
             <h3>Countries : {countries.length}</h3>
+
+            <div>
+                <h3>Visited Countries : {visitedCountries.length}</h3>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
             <div className="country-container">
             {
               countries.map(country => <Country 
               key={country.cca3} 
+              handleVisitedCountry={handleVisitedCountry}
               country={country}>
 
               </Country>)
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country ,handleVisitedCountry}) => {
 
     const {name,flags,population,area, cca3} = country;
 
@@ -9,14 +9,22 @@ const Country = ({ country }) => {
         setVisited(!visited);
     }
 
+    const passParams = () =>{
+        handleVisitedCountry(country);
+    }
+
+
     return (
-        <div className='country'>
-            <h3>Name : {name?.common}</h3>
+        <div  className={`country ${visited && 'visited'}`}>
+            <h3 style={{color: visited? 'black' : 'white'}}>Name : {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population : {population}</p>
             <p>Area : {area}</p>
             <p><small>Code : {cca3}</small></p>
+            <button onClick={passParams}>Mark Visited</button>
+            <br />
             <button onClick={handleVisited}>Visited</button>{visited && 'visited'}
+            
         </div>
     );
 };
